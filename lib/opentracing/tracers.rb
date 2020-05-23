bcc9328d201d17b7752518b34732d0be0a71c9c2
Marcos Minond <minond.marcos@gmail.com>
2020-05-23 13:56:58 -0600

    jaeger client helper

--- lib/opentracing/tracers.rb
+++ lib/opentracing/tracers.rb
@@ -2,6 +2,47 @@ require "opentracing/tracers/version"
 
 module OpenTracing
   module Tracers
-    class Error < StandardError; end
+    # @param [String] service_name
+    # @param [Hash] config
+    # @param [Jaeger::Sampler] sampler
+    # @param [Jaeger::Client::Reporter] reporter
+    # @return [Jaeger::Client]
+    def self.build_jaeger_client(
+      service_name,
+      config: Rails.application.config.jaeger,
+      sampler: build_jaeger_sampler,
+      reporter: build_jaeger_reporter(service_name, config)
+    )
+      Jaeger::Client.build(
+        :host => config[:host],
+        :port => config[:port],
+        :service_name => service_name,
+        :sampler => sampler,
+        :reporter => reporter,
+      )
+    end
+
+    # @return [Jaeger::Samplers::Const]
+    def self.build_jaeger_sampler
+      Jaeger::Samplers::Const.new(true)
+    end
+
+    # @param [String] service_name
+    # @param [Hash] config
+    # @return [Jaeger::Client::Reporters::RemoteReporter]
+    def self.build_jaeger_reporter(service_name, config)
+      Jaeger::Client::Reporters::RemoteReporter.new(
+        :flush_interval => config[:flush_interval],
+        :sender => Jaeger::UdpSender.new(
+          :host => config[:host],
+          :port => config[:port],
+          :max_packet_size => config[:max_packet_size],
+          :logger => Logger.new(STDOUT),
+          :encoder => Jaeger::Encoders::ThriftEncoder.new(
+            :service_name => service_name
+          )
+        )
+      )
+    end
   end
 end
